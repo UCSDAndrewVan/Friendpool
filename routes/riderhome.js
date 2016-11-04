59afbf582d141ee5da923e01fccfbc70a3684e27
Tim Banh <tim-banh@outlook.com>
2016-11-03 20:19:05 -0700

    Unavailable people are unclickable

--- routes/riderhome.js
+++ routes/riderhome.js
@@ -2,7 +2,13 @@ $.getJSON("../data.json",
 function (data){
     var frame = $("#frame");
     for (var i = 0; i < data.friends.length; i++) {
-        var row = $('<div class="row ride">');
+        
+        if (data.friends[i].depart == "UNAVAILABLE") {
+            var row = $('<div class="row ride" id="unavailable">');
+        } else {
+            var row = $('<div class="row ride">');
+        }
+        
         row.append('<div class="col-sm-4"><p2>'+ data.friends[i].depart + "</p2></div>");
         row.append('<div class="col-sm-2" value="' + data.friends[i].imageURL + '"><img src="'+ data.friends[i].imageURL + '"></div>');
         row.append('<div class="col-sm-6"><p1>' + data.friends[i].name + "</p1></div>");
@@ -11,10 +17,14 @@ function (data){
     }
 });
 
-$(document).on("click", '.ride', function( ) {
-    window.location = "../static/confirm.html";
+$(document).on("click", '.ride', function() {
+    var time = $(this).find(".col-sm-4 p2").text();
+    if (time == "UNAVAILABLE") {
+        return;
+    }
     var name = $(this).find(".col-sm-6 p1").text();
     var img = $(this).find(".col-sm-2").attr("value");
     sessionStorage.setItem('name', name);
     sessionStorage.setItem('img', img);
+    window.location = "../static/confirm.html";
 });
